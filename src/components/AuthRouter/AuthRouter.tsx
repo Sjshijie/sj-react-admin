84dc718cc04a89f4df532ab82810710374fc10f9
Stepan-sj <a66377172@qq.com>
2021-05-30 17:34:19 +0800

    update

--- src/components/AuthRouter/AuthRouter.tsx
+++ src/components/AuthRouter/AuthRouter.tsx
@@ -9,7 +9,7 @@ function RouterArr(props:any) {
           const Component = routeComponents[item.name]
           if(Component){
             return (<Route path={item.path} key={item.name} exact={item.children && item.children.length?false:true} render={() => {
-              return <Component childrenRoutes={item.children}/>
+              return <Component childrenRoutes={item.children} title={item.title}/>
             }}></Route>)
           }
           
--- src/page/qualificationReview/index.tsx
+++ src/page/qualificationReview/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, Suspense,useState } from 'react';
 import AuthRouter from '../../components/AuthRouter/AuthRouter'
 
 export default function (props:any){
+    console.log(props)
     return (
         <Suspense fallback='loading'>
             <AuthRouter routes={props.childrenRoutes}/>
